b50644d047feb1ffb0b671b0f51a6e1295222529
Yunkoo-GIT <ykkangkr@gmail.com>
2022-05-12 23:24:54 +0900

    Update 3

---
+++ 21-1 딥러닝 실제/과제/Image(matplotlib)/Report-04.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 24 16:53:55 2021
+
+@author: kadmin
+"""
+
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+
+img = imread('Yunkoo.jpg')
+
+plt.imshow(img)
+plt.show()
